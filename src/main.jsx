90715f7f84c308cb1daead9a55564abaefd37205
agethejedi <114832589+agethejedi@users.noreply.github.com>
2026-05-07 20:09:55 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import JarvisBriefing from "./JarvisBriefing.live.jsx";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
